9d0864abbb9e1c4563b79f8b31d617a9c7c31c18
Florian Bieck <info@florianbieck.com>
2025-10-16 00:43:01 +0200

    Throw domain error if user by keycloak user id not found

--- apps/main/src/users/infrastructure/inject-user-to-auth-context.guard.ts
+++ apps/main/src/users/infrastructure/inject-user-to-auth-context.guard.ts
@@ -1,6 +1,7 @@
 import type { CanActivate, ExecutionContext } from "@nestjs/common";
 import type { AuthRequest } from "@open-dpp/auth";
 import { Injectable } from "@nestjs/common";
+import { NotFoundError } from "@open-dpp/exception";
 import { UsersService } from "./users.service";
 
 @Injectable()
@@ -14,9 +15,13 @@ export class InjectUserToAuthContextGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest() as AuthRequest;
     if (request.authContext) {
-      request.authContext.user = await this.usersService.findByKeycloakUserId(
+      const user = await this.usersService.findByKeycloakUserId(
         request.authContext.keycloakUser.sub,
       );
+      if (!user) {
+        throw new NotFoundError("User not found");
+      }
+      request.authContext.user = user;
     }
     return true;
   }
