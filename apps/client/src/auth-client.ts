b0b3ff3ca413184fbb6026d5e69e56351f4870c1
Florian Bieck <info@florianbieck.com>
2025-10-27 09:23:48 +0100

    fix: use env for auth url

--- apps/client/src/auth-client.ts
+++ apps/client/src/auth-client.ts
@@ -1,9 +1,10 @@
 import { genericOAuthClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/vue";
+import { API_URL } from "./const.ts";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: `${API_URL}/auth`,
   plugins: [
     genericOAuthClient(),
   ],
