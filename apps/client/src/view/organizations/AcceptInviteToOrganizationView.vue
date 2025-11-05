1f32da34a93afd9b50a80dd4fc9ee3859173afbf
Florian Bieck <info@florianbieck.com>
2025-11-05 17:11:32 +0100

    fix: Remove type="submit" from button outside a form.

--- apps/client/src/view/organizations/AcceptInviteToOrganizationView.vue
+++ apps/client/src/view/organizations/AcceptInviteToOrganizationView.vue
@@ -47,7 +47,7 @@ async function acceptInvite() {
           </div>
 
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500" @click="acceptInvite">
+            <button type="button" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500" @click="acceptInvite">
               {{ t('organizations.invitation.accept') }}
             </button>
           </div>
