07533af767a9735812860d7b0ea2308527517864
Florian Bieck <info@florianbieck.com>
2025-12-16 10:43:41 +0100

    refactor(api-client): remove unused TypeScript declaration output configuration from Rollup

--- packages/api-client/rollup.config.js
+++ packages/api-client/rollup.config.js
@@ -2,17 +2,6 @@ import typescript from '@rollup/plugin-typescript'
 
 export default [
   {
-    input: './src/index.ts', // the entry declaration file from tsc
-    output: [
-      {
-        file: './dist/index.d.ts',
-        format: 'es',
-      },
-    ],
-    plugins: [typescript()],
-    external: ['axios'],
-  },
-  {
     input: './src/index.ts',
     output: {
       file: './dist/index.js',
