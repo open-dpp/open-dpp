04c00d8583f9bb22ad2ce40467382ae3958d7b7c
Florian Bieck <info@florianbieck.com>
2026-01-09 13:14:24 +0100

    feat(admin): implement user management with invite functionality
    - Replaced `OrganizationsAdminList` with `AdminUsersList` in `UsersAdminView.vue`.
    - Added `InviteUserDialog` for inviting new users with admin role.
    - Updated user fetching logic to utilize `authClient.admin.listUsers`.
    - Introduced conditional modal rendering for inviting users.

---
+++ apps/client/src/components/admin/InviteUserDialog.vue
@@ -0,0 +1,209 @@
+<script lang="ts" setup>
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import { EnvelopeIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import Message from "primevue/message";
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { authClient } from "../../auth-client.ts";
+import RingLoader from "../RingLoader.vue";
+
+const emit = defineEmits<{
+  (e: "close"): void;
+  (e: "invitedUser"): void;
+}>();
+const { t } = useI18n();
+const loading = ref<boolean>(false);
+const errors = ref<Array<string>>([]);
+const success = ref<boolean>(false);
+const email = ref("");
+const password = ref("");
+const firstName = ref("");
+const lastName = ref("");
+
+async function inviteUser() {
+  success.value = false;
+  errors.value = [];
+
+  try {
+    loading.value = true;
+    const fullName = `${firstName.value} ${lastName.value}`;
+    const { error } = await authClient.admin.createUser({
+      email: email.value,
+      password: password.value,
+      name: fullName,
+      role: "admin",
+      data: {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        emailVerified: true,
+      },
+    });
+    loading.value = false;
+    if (!error) {
+      success.value = true;
+      emit("invitedUser");
+      email.value = ""; // Reset form
+    }
+    else {
+      errors.value.push("Ein Fehler ist aufgetreten.");
+    }
+  }
+  catch (error) {
+    console.error(error);
+    errors.value.push("Ein Fehler ist aufgetreten.");
+    loading.value = false;
+  }
+}
+</script>
+
+<template>
+  <TransitionRoot :show="true" as="template">
+    <Dialog class="relative z-10" @close="emit('close')">
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-gray-500/75 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div
+          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <DialogPanel
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6"
+            >
+              <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+                <button
+                  class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-hidden focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  type="button"
+                  @click="emit('close')"
+                >
+                  <span class="sr-only">{{ t('common.close') }}</span>
+                  <XMarkIcon aria-hidden="true" class="size-6" />
+                </button>
+              </div>
+              <div>
+                <div
+                  class="mx-auto flex size-12 items-center justify-center rounded-full bg-green-100"
+                >
+                  <EnvelopeIcon
+                    aria-hidden="true"
+                    class="size-6 text-green-600"
+                  />
+                </div>
+                <div class="mt-3 text-center sm:mt-5">
+                  <DialogTitle
+                    as="h3"
+                    class="text-base font-semibold text-gray-900"
+                  >
+                    {{ t('organizations.admin.inviteUserDialog.title') }}
+                  </DialogTitle>
+                  <div v-if="success" class="mt-3">
+                    <div class="text-sm text-green-600">
+                      {{ t('organizations.admin.inviteUserDialog.success') }}
+                    </div>
+                    <button
+                      class="mt-3 rounded-md bg-green-600 text-white px-3 py-2 text-sm font-semibold shadow-xs hover:bg-green-700 focus:outline-hidden focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                      type="button"
+                      @click="emit('close')"
+                    >
+                      {{ t('common.close') }}
+                    </button>
+                  </div>
+                  <div v-else class="mt-3">
+                    <form v-show="!loading" class="flex flex-col gap-4" @submit.prevent="inviteUser">
+                      <div v-if="errors.length" class="flex flex-col gap-1">
+                        <Message v-for="error in errors" :key="error" severity="error" :closable="false">
+                          {{ error }}
+                        </Message>
+                      </div>
+
+                      <div class="flex flex-col gap-2">
+                        <label for="email" class="block text-sm font-medium text-gray-700">
+                          {{ t('common.form.email.label') }}
+                        </label>
+                        <InputText
+                          id="email"
+                          v-model="email"
+                          type="text"
+                          class="w-full"
+                        />
+                      </div>
+
+                      <div class="flex flex-col gap-2">
+                        <label for="password" class="block text-sm font-medium text-gray-700">
+                          {{ t('common.form.password.label') }}
+                        </label>
+                        <InputText
+                          id="password"
+                          v-model="password"
+                          type="password"
+                          class="w-full"
+                        />
+                      </div>
+
+                      <div class="flex flex-col gap-2">
+                        <label for="email" class="block text-sm font-medium text-gray-700">
+                          {{ t('user.firstName') }}
+                        </label>
+                        <InputText
+                          id="firstName"
+                          v-model="firstName"
+                          type="text"
+                          class="w-full"
+                        />
+                      </div>
+
+                      <div class="flex flex-col gap-2">
+                        <label for="email" class="block text-sm font-medium text-gray-700">
+                          {{ t('user.lastName') }}
+                        </label>
+                        <InputText
+                          id="lastName"
+                          v-model="lastName"
+                          type="text"
+                          class="w-full"
+                        />
+                      </div>
+
+                      <Button
+                        :label="t('organizations.admin.inviteUserDialog.create')"
+                        type="submit"
+                        :loading="loading"
+                        class="w-full"
+                      />
+                    </form>
+                    <RingLoader v-show="loading" class="mx-auto w-fit" />
+                  </div>
+                </div>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
--- apps/client/src/view/admin/UsersAdminView.vue
+++ apps/client/src/view/admin/UsersAdminView.vue
@@ -1,66 +1,41 @@
 <script lang="ts" setup>
-import type { User } from "better-auth";
-import { computed, onMounted, ref } from "vue";
-import { useI18n } from "vue-i18n";
-import { useRoute } from "vue-router";
+import type { UserWithRole } from "better-auth/plugins";
+import { onMounted, ref } from "vue";
 import { authClient } from "../../auth-client.ts";
-import OrganizationsAdminList from "../../components/organizations/OrganizationsAdminList.vue";
-import apiClient from "../../lib/api-client";
+import AdminUsersList from "../../components/admin/AdminUsersList.vue";
+import InviteUserDialog from "../../components/admin/InviteUserDialog.vue";
+import { ModalType, useLayoutStore } from "../../stores/layout.ts";
 
-const { t } = useI18n();
-const route = useRoute();
-const buttonLabel = computed(() => t("items.new"));
+const layoutStore = useLayoutStore();
 
-const users = ref<User[]>([]);
+const users = ref<UserWithRole[]>([]);
 
 async function fetchUsers() {
-  authClient.admin.listUsers();
   const res = await authClient.admin.listUsers({
     query: {},
   });
   if (res.data) {
-    users.value = res.data;
+    users.value = res.data.users;
   }
 }
 
 async function onAdd() {
-  await apiClient.dpp.items.create(String(route.params.modelId));
-  await fetchOrganizations();
+  layoutStore.openModal(ModalType.INVITE_USER_MODAL);
 }
 
 onMounted(async () => {
-  await fetchOrganizations();
+  await fetchUsers();
 });
 </script>
 
 <template>
   <section>
     <div class="flex flex-col gap-3 p-3">
-      <OrganizationsAdminList v-if="organizations.length > 0" :organizations="organizations" @add="onAdd" />
-      <button
-        v-else
-        class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-hidden focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        type="button"
-        @click="onAdd"
-      >
-        <svg
-          aria-hidden="true"
-          class="mx-auto h-12 w-12 text-gray-400"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 48 48"
-        >
-          <path
-            d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          />
-        </svg>
-        <span class="mt-2 block text-sm font-semibold text-gray-900">{{
-          buttonLabel
-        }}</span>
-      </button>
+      <InviteUserDialog
+        v-if="layoutStore.modalOpen === ModalType.INVITE_USER_MODAL"
+        @close="layoutStore.closeModal()"
+      />
+      <AdminUsersList v-if="users.length > 0" :users="users" @add="onAdd" />
     </div>
   </section>
 </template>
