2134479cbb67c3052f5336fe2acfaa691ec4709c
Florian Bieck <info@florianbieck.com>
2026-01-19 23:31:23 +0100

    refactor(dto): extract `BaseCreateModelDtoSchema` for improved reusability

--- apps/main/src/models/presentation/dto/update-model.dto.ts
+++ apps/main/src/models/presentation/dto/update-model.dto.ts
@@ -1,6 +1,8 @@
 import type { z } from "zod";
-import { CreateModelDtoSchema } from "./create-model.dto";
+import {
+  BaseCreateModelDtoSchema,
+} from "./create-model.dto";
 
-export const UpdateModelDtoSchema = CreateModelDtoSchema.partial();
+export const UpdateModelDtoSchema = BaseCreateModelDtoSchema.partial();
 
 export type UpdateModelDto = z.infer<typeof UpdateModelDtoSchema>;
