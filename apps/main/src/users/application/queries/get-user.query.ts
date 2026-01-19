946f270c27b9798be8485475c248f53e77303665
Florian Bieck <info@florianbieck.com>
2026-01-19 11:03:06 +0100

    feat(users): add CQRS commands and queries for user creation and retrieval
    - Introduced `CreateUserCommand` to handle user creation.
    - Added `GetUserQuery` to retrieve user details.

---
+++ apps/main/src/users/application/queries/get-user.query.ts
@@ -0,0 +1,10 @@
+import { Query } from "@nestjs/cqrs";
+import { User } from "../../domain/user";
+
+export class GetUserQuery extends Query<User | null> {
+    constructor(
+        public readonly userId: string,
+    ) {
+        super();
+    }
+}
