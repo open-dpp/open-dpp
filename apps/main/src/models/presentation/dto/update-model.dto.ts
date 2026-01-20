745cb7ed6d1f41c8b2a49faa702eb815d8dd20c5
Florian Bieck <info@florianbieck.com>
2026-01-20 09:57:51 +0100

    refactor(dto): add validation to ensure `templateId` and `marketplaceResourceId` are mutually exclusive

--- apps/main/src/models/presentation/dto/update-model.dto.ts
+++ apps/main/src/models/presentation/dto/update-model.dto.ts
@@ -3,6 +3,11 @@ import {
   BaseCreateModelDtoSchema,
 } from "./create-model.dto";
 
-export const UpdateModelDtoSchema = BaseCreateModelDtoSchema.partial();
+export const UpdateModelDtoSchema = BaseCreateModelDtoSchema.partial().refine(
+  data => !(data.templateId && data.marketplaceResourceId),
+  {
+    error: "marketplaceResourceId and templateId are mutually exclusive",
+  },
+);
 
 export type UpdateModelDto = z.infer<typeof UpdateModelDtoSchema>;
