2b3b73f35886773fa65ab4862a47a467c1ca7e8c
Florian Bieck <info@florianbieck.com>
2026-01-20 14:48:23 +0100

    refactor(module): reorder imports in `organizations.module` for consistency

--- apps/main/src/organizations/organizations.module.ts
+++ apps/main/src/organizations/organizations.module.ts
@@ -1,22 +1,22 @@
 import { forwardRef, Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
 import { CqrsModule } from "@nestjs/cqrs";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AuthModule } from "../auth/auth.module";
 import { EmailModule } from "../email/email.module";
 import { UsersModule } from "../users/users.module";
-import { OrganizationsRepositoryPort } from "./domain/ports/organizations.repository.port";
-import { OrganizationsRepository } from "./infrastructure/adapters/organizations.repository";
-import { MembersRepositoryPort } from "./domain/ports/members.repository.port";
-import { MembersRepository } from "./infrastructure/adapters/members.repository";
-import { OrganizationMapper } from "./infrastructure/mappers/organization.mapper";
-import { MemberMapper } from "./infrastructure/mappers/member.mapper";
-import { Organization, OrganizationSchema } from "./infrastructure/schemas/organization.schema";
-import { Member, MemberSchema } from "./infrastructure/schemas/member.schema";
 import { CreateOrganizationCommandHandler } from "./application/commands/create-organization.command-handler";
-import { UpdateOrganizationCommandHandler } from "./application/commands/update-organization.command-handler";
-import { GetOrganizationQueryHandler } from "./application/queries/get-organization.query-handler";
-import { GetMembersQueryHandler } from "./application/queries/get-members.query-handler";
 import { InviteMemberCommandHandler } from "./application/commands/invite-member.command-handler";
+import { UpdateOrganizationCommandHandler } from "./application/commands/update-organization.command-handler";
+import { GetMembersQueryHandler } from "./application/queries/get-members.query-handler";
+import { GetOrganizationQueryHandler } from "./application/queries/get-organization.query-handler";
+import { MembersRepositoryPort } from "./domain/ports/members.repository.port";
+import { OrganizationsRepositoryPort } from "./domain/ports/organizations.repository.port";
+import { MembersRepository } from "./infrastructure/adapters/members.repository";
+import { OrganizationsRepository } from "./infrastructure/adapters/organizations.repository";
+import { MemberMapper } from "./infrastructure/mappers/member.mapper";
+import { OrganizationMapper } from "./infrastructure/mappers/organization.mapper";
+import { Member, MemberSchema } from "./infrastructure/schemas/member.schema";
+import { Organization, OrganizationSchema } from "./infrastructure/schemas/organization.schema";
 import { OrganizationsController } from "./presentation/organizations.controller";
 
 @Module({
