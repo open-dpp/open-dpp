bdf304a505cc04a635cc1925f5f57c5dec37be64
Florian Bieck <info@florianbieck.com>
2026-01-26 18:38:00 +0100

    refactor(zod-validation-pipe): clean up code formatting and simplify transform method
    - Adjusted formatting for better readability and consistency.
    - Removed unused `metadata` parameter from the `transform` method.
    - Simplified error handling and parsing logic.

--- apps/main/src/common/pipes/zod-validation.pipe.ts
+++ apps/main/src/common/pipes/zod-validation.pipe.ts
@@ -1,16 +1,15 @@
-
-import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
-import { ZodSchema } from 'zod';
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { ZodSchema } from "zod";
 
 export class ZodValidationPipe implements PipeTransform {
-    constructor(private schema: ZodSchema) { }
+  constructor(private schema: ZodSchema) { }
 
-    transform(value: unknown, metadata: ArgumentMetadata) {
-        try {
-            const parsedValue = this.schema.parse(value);
-            return parsedValue;
-        } catch (error) {
-            throw new BadRequestException('Validation failed');
-        }
+  transform(value: unknown, _: ArgumentMetadata) {
+    try {
+      return this.schema.parse(value);
     }
+    catch {
+      throw new BadRequestException("Validation failed");
+    }
+  }
 }
