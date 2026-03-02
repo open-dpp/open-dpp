dbf9a122b1bb8f4dc7cc4a224f8908ad4d456afd
Henri Burau <henri.burau@web.de>
2026-03-02 17:06:40 +0100

    feat: allow unset values for submodel elements

--- apps/client/src/components/presentation/SubmodelElementValue.vue
+++ apps/client/src/components/presentation/SubmodelElementValue.vue
@@ -4,6 +4,7 @@ import type {
   SubmodelElementCollectionResponseDto,
   SubmodelElementResponseDto,
 } from "@open-dpp/dto";
+import { useI18n } from "vue-i18n";
 import MediaFieldView from "../media/MediaFieldView.vue";
 import List from "./List.vue";
 import Reference from "./Reference.vue";
@@ -12,6 +13,8 @@ import Subsection from "./SubmodelElementCollection.vue";
 const { element } = defineProps<{
   element: SubmodelElementResponseDto;
 }>();
+
+const { t } = useI18n();
 </script>
 
 <template>
@@ -21,17 +24,25 @@ const { element } = defineProps<{
   >
     {{ element.value }}
   </dd>
-  <Reference v-if="element.modelType === 'ReferenceElement'" :model="element.value as ReferenceValue" />
+  <div v-if="!element.value" class="mt-1 text-sm/6 font-semibold text-red-800 sm:mt-2">
+    {{ t("presentation.missingValue") }}
+  </div>
+  <Reference
+    v-else-if="element.modelType === 'ReferenceElement'"
+    :model="element.value as ReferenceValue"
+  />
   <MediaFieldView
-    v-if="element.modelType === 'File' && typeof element.value === 'string'"
+    v-else-if="
+      element.modelType === 'File' && typeof element.value === 'string'
+    "
     :media-id="element.value"
   />
   <List
-    v-if="element.modelType === 'SubmodelElementList'"
+    v-else-if="element.modelType === 'SubmodelElementList'"
     :content="element.value as SubmodelElementCollectionResponseDto[]"
   />
   <Subsection
-    v-if="element.modelType === 'SubmodelElementCollection'"
+    v-else-if="element.modelType === 'SubmodelElementCollection'"
     :id-short="element.idShort"
   />
 </template>
