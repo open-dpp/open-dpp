45a08421c37989382d726b47155fe869925d8962
mrudolph <michael.rudolph@open-dpp.de>
2026-03-03 08:56:04 +0100

    Remove `hasUniqueLanguagesOrFail` validation from `displayName` setters across submodel elements.

--- apps/main/src/aas/domain/submodel-base/submodel-element-list.ts
+++ apps/main/src/aas/domain/submodel-base/submodel-element-list.ts
@@ -39,7 +39,6 @@ export class SubmodelElementList implements ISubmodelElement {
   }
 
   set displayName(value: Array<LanguageText>) {
-    hasUniqueLanguagesOrFail(value);
     this._displayName = value;
   }
 
