b4c4ccf19e386a6ba0314823aa39ec60de98ab73
Henri Burau <henri.burau@web.de>
2026-05-13 16:11:58 +0200

    wip

--- apps/client/src/components/basics/LanguageSelect.vue
+++ apps/client/src/components/basics/LanguageSelect.vue
@@ -1,19 +1,44 @@
 <script setup lang="ts">
-import type { LanguageType } from "@open-dpp/dto";
-import { Language } from "@open-dpp/dto";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
+import { search } from "language-tags";
 
-const props = defineProps<{ ignoreOptions: LanguageType[]; disabled?: boolean }>();
+const props = defineProps<{ ignoreOptions: string[]; disabled?: boolean }>();
 const model = defineModel();
-const { t } = useI18n();
+const { t, locale } = useI18n();
 
-const languageOptions = computed(() => {
-  return [
-    { name: t("languages.english"), language: Language.en },
-    { name: t("languages.german"), language: Language.de },
-  ].filter((option) => !props.ignoreOptions.includes(option.language));
-});
+interface LanguageTag {
+  tag: string;
+  description: string;
+}
+
+const languageNames = new Intl.DisplayNames([locale.value], { type: "language" });
+
+const initialOptions = (): LanguageTag[] => {
+  const tags = [...new Set([...navigator.languages, "de-DE", "en-US"])];
+
+  return tags.map((tag) => ({
+    tag: tag,
+    description: languageNames.of(tag) ?? t("language.unknown"),
+  }));
+};
+
+const languageOptions = ref(initialOptions());
+
+const searchLanguage = (language: string) => {
+  if (language.length === 0) {
+    languageOptions.value = initialOptions();
+    return;
+  }
+
+  const tags = search(language).filter((t) => t.type() === "language");
+
+  languageOptions.value = tags.slice(0, Math.min(tags.length, 5)).map((tag) => ({
+    tag: tag.format(),
+    description: tag.descriptions()[0] ?? t("language.unknown")
+  }));
+
+};
 </script>
 
 <template>
@@ -21,8 +46,10 @@ const languageOptions = computed(() => {
     v-model="model"
     :disabled="props.disabled"
     :options="languageOptions"
-    option-value="language"
-    option-label="name"
+    filter
+    @filter="searchLanguage($event.value)"
+    option-value="tag"
+    option-label="description"
     placeholder="Select a Language"
   />
 </template>
